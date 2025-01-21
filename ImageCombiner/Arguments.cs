8a5481ee2ee9048f263b2e8828106b6f9dfd563a
Jamez <Jamez@DESKTOP-A74GUR0>
2025-01-20 21:24:17 -0500

    Cleaned up argument parsing.

---
+++ ImageCombiner/Arguments.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageCombiner
+{
+    internal class Arguments
+    {
+        public bool ShowHelp { get; }
+        public string? ErrorText { get; }
+
+        public string? SourceFolder { get; }
+        public string? TargetFolder { get; }
+
+        public int RowSize { get; } = 10;
+        public int MaxRows { get; } = 5;
+
+        public int MarginX { get; } = 0;
+        public int MarginY { get; } = 0;
+
+        internal Arguments(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1 && (string.Equals(args[0], "-h") || string.Equals(args[0], "-help")))
+                {
+                    ShowHelp = true;
+                    return;
+                }
+
+                for (var currentArg = 0; currentArg < args.Length; currentArg++)
+                {
+                    if (string.Equals(args[currentArg], "-RowSize", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        RowSize = ParseInt(args, currentArg + 1, "-RowSize");
+                        currentArg++;
+                        continue;
+                    }
+
+                    if (string.Equals(args[currentArg], "-MaxRows", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        MaxRows = ParseInt(args, currentArg + 1, "-MaxRows");
+                        currentArg++;
+                        continue;
+                    }
+
+                    if (string.Equals(args[currentArg], "-MarginX", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        MarginX = ParseInt(args, currentArg + 1, "-MarginX");
+                        currentArg++;
+                        continue;
+                    }
+
+                    if (string.Equals(args[currentArg], "-MarginY", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        MarginY = ParseInt(args, currentArg + 1, "-MarginY");
+                        currentArg++;
+                        continue;
+                    }
+
+                    // If we aren't parsing a flag, then it must be the source and then target folders.
+                    if (SourceFolder == null)
+                    {
+                        SourceFolder = args[currentArg];
+                    }
+                    else TargetFolder = args[currentArg];
+                }
+
+                if (SourceFolder == null)
+                {
+                    ErrorText = "Source Folder not specified";
+                    return;
+                }
+
+                if (!Path.IsPathFullyQualified(SourceFolder))
+                {
+                    SourceFolder = Path.Join(Directory.GetCurrentDirectory(), SourceFolder);
+                }
+
+                if (TargetFolder != null)
+                {
+                    if (!Path.IsPathFullyQualified(TargetFolder))
+                    {
+                        TargetFolder = Path.Join(Directory.GetCurrentDirectory(), TargetFolder);
+                    }
+                }
+                else
+                {
+                    var targetFolderDI = new DirectoryInfo(SourceFolder);
+                    if (targetFolderDI == null || targetFolderDI.Parent == null) throw new ArgumentException("Somehow the Source Folder doesn't exist");
+                    TargetFolder = targetFolderDI.Parent.FullName;
+                    Console.WriteLine($"Target Folder not specified, using Parent of source folder: {TargetFolder}");
+                }
+            }
+            catch (Exception ex)
+            {
+                ErrorText = ex.Message;
+            }
+        }
+
+        private int ParseInt(string[] args, int valueArg, string argName) 
+        {
+            if (valueArg >= args.Length)
+            {
+                throw new ArgumentException($"{argName} flag used without value");
+            }
+
+            try
+            {
+                return int.Parse(args[valueArg]);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"Error parsing value {args[valueArg]} for Argument {argName}", ex);
+            }
+        }
+    }
+}
--- ImageCombiner/Program.cs
+++ ImageCombiner/Program.cs
@@ -1,77 +1,164 @@
-﻿using System;
-using System.IO;
-using System.Drawing;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ImageCombiner;
+
+internal class Program
 {
-    internal class Program
+    private const float MAX_WIDTH = 4096;
+
+    private static Arguments? arguments;
+
+    static void Main(string[] args)
     {
-        private const int RowSize = 10;
-        private const int NumRows = 5;
-
-        private static List<string> folders = new List<string> { "Cards" };
-        private const string parentFolder = "c:\\Games\\SnoopCereal";
-        private const int margin_x = 90;
-        private const int margin_y = 90;
-
-        static void Main(string[] args)
+        arguments = new Arguments(args);
+        if (arguments.ShowHelp)
         {
-            foreach (var folder in folders)
-            {
-                CombineFolder(folder);
-            }
+            Help();
+            return;
         }
 
-        static void CombineFolder(string inputFolderName)
+        if (arguments.ErrorText != null)
         {
-            var fullPath = Path.Combine(parentFolder, inputFolderName);
-            var di = new DirectoryInfo(fullPath);
-            var counter = 0;
-            var files = di.EnumerateFiles().Select(fi => fi.FullName);
-            var filesPerPage = RowSize * NumRows;
-            while (true)
-            {
-                var batch = files.Skip(counter * filesPerPage).Take(filesPerPage).ToList();
-                if (batch.Count == 0) break;
-
-                var filePath = Path.Combine(parentFolder, inputFolderName);
-                if (counter > 0) filePath += $"{counter}";
-                filePath += ".png";
-
-                Combine(batch, filePath);
-                counter++;
-            }
+            Console.WriteLine("Error parsing Arguments: " + arguments.ErrorText);
+            Usage();
+            return;
         }
 
-        static void Combine(List<string> fileNames, string outputFilename)
+        if (arguments.SourceFolder == null || arguments.TargetFolder == null)
         {
-            var image = Image.FromFile(fileNames[0]);
-            var width = image.Width - (2 * margin_x);
-            var height = image.Height - (2 * margin_y);
+            Console.WriteLine("Source or Target Folder isn't specified");
+            Usage();
+            return;
+        }
 
-            var rowCount = (int)Math.Ceiling((double)fileNames.Count / RowSize);
+        if (!EnsureFolders(arguments))
+        {
+            Console.WriteLine("Source or Target folder doesn't exist or isn't specified");
+            Usage();
+            return;
+        }
 
-            using (var bitmap = new Bitmap(width * RowSize, height * rowCount))
+        Console.WriteLine($"Creating {arguments.RowSize} cards per row, max {arguments.MaxRows} rows");
+
+        CombineFolder(arguments.SourceFolder, arguments.TargetFolder);
+    }
+
+    static void Usage()
+    {
+        Console.WriteLine("Usage: ImageCombiner (-RowSize <row-size>) (-MaxRows <max-rows>) (-MarginX <margin-x>) (-MarginY <margin-y>) <source-folder> (<output-folder>)");
+        Console.WriteLine("If <output-folder> is omitted, output files are left in the parent of the source folder");
+        Console.WriteLine("Default number of cards per row is 10. Default max number of rows is 5");
+        Console.WriteLine("If Margin_X or Margin_Y are nonzero, that many pixels will be removed from the top and bottom of each card image");
+    }
+
+    static void Help()
+    {
+        Usage();
+
+        Console.WriteLine("ImageCombiner is designed to work with Tabletop Simulator to combine multiple images from a single folder into one or more");
+        Console.WriteLine("Image files with 10 images per row and a maximum of 5 rows. If there are more than 50 cards, it will create multiple output");
+        Console.WriteLine("Files.");
+        Console.WriteLine("If the total image size is too big for Tabletop Simulator, it will scale the cards down to an acceptable size");
+    }
+
+    static private bool EnsureFolders(Arguments arguments)
+    {
+        if (arguments.SourceFolder == null || arguments.TargetFolder == null) return false;
+
+        Console.WriteLine($"Using Source Folder {arguments.SourceFolder}");
+        if (!Directory.Exists(arguments.SourceFolder))
+        {
+            Console.WriteLine($"Source Folder {arguments.SourceFolder} Doesn't Exist. Creating");
+            Directory.CreateDirectory(arguments.SourceFolder);
+
+        }
+
+        Console.WriteLine($"Using Target Folder {arguments.TargetFolder}");
+        if (!Directory.Exists(arguments.TargetFolder))
+        {
+            Console.WriteLine($"Target Folder {arguments.TargetFolder} Doesn't Exist. Creating");
+            Directory.CreateDirectory(arguments.TargetFolder);
+        }
+        return true;
+    }
+
+
+    static private void CombineFolder(string sourceFolder, string targetFolder)
+    {
+        var di = new DirectoryInfo(sourceFolder);
+        var counter = 0;
+        var files = di.EnumerateFiles().Select(fi => fi.FullName);
+        var filesPerPage = arguments.RowSize * arguments.MaxRows;
+        while (true)
+        {
+            var batch = files.Skip(counter * filesPerPage).Take(filesPerPage).ToList();
+            if (batch.Count == 0) break;
+
+            var filePath = Path.Combine(targetFolder, di.Name);
+            if (counter > 0) filePath += $"{counter}";
+            filePath += ".png";
+
+            Console.WriteLine($"Creating {filePath}");
+
+            Combine(batch, filePath);
+            counter++;
+        }
+    }
+
+    static void Combine(List<string> fileNames, string outputFilename)
+    {
+        var image = Image.FromFile(fileNames[0]);
+        var width = image.Width - (2 * arguments.MarginX);
+        var height = image.Height - (2 * arguments.MarginY);
+
+        var rowCount = (int)Math.Ceiling((double)fileNames.Count / arguments.RowSize);
+
+        using (var bitmap = new Bitmap(width * arguments.RowSize, height * rowCount))
+        {
+            using (var canvas = Graphics.FromImage(bitmap))
             {
-                using (var canvas = Graphics.FromImage(bitmap))
+                canvas.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+                for (var rowNum = 0; rowNum < rowCount; rowNum++)
                 {
-                    canvas.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-
-                    for (var rowNum = 0; rowNum < rowCount; rowNum++)
+                    var hOffset = 0;
+                    foreach (var sourceImagePath in fileNames.Skip(arguments.RowSize *rowNum).Take(arguments.RowSize))
                     {
-                        var hOffset = 0;
-                        foreach (var sourceImagePath in fileNames.Skip(RowSize*rowNum).Take(RowSize))
-                        {
-                            var sourceImage = Image.FromFile(sourceImagePath);
-                            canvas.DrawImage(sourceImage, new Rectangle(hOffset, rowNum * height, width, height), new Rectangle(margin_x, margin_y, sourceImage.Width - (2* margin_x), sourceImage.Height - (2*margin_y)), GraphicsUnit.Pixel);
-                            hOffset += width;
-                        }
+                        var sourceImage = Image.FromFile(sourceImagePath);
+                        canvas.DrawImage(sourceImage, new Rectangle(hOffset, rowNum * height, width, height), new Rectangle(arguments.MarginX, arguments.MarginY, sourceImage.Width - (2* arguments.MarginX), sourceImage.Height - (2*arguments.MarginY)), GraphicsUnit.Pixel);
+                        hOffset += width;
                     }
-                    canvas.Save();
                 }
+                canvas.Save();
+            }
+
+            // If the results are more than 4k pixels wide, we need to scale it down.
+            var totalWidth = width * arguments.RowSize;
+            var totalHeight = height * rowCount;
+            if (totalWidth > MAX_WIDTH)
+            {
+                var ratio = .25; //MAX_WIDTH / totalWidth;
+                var newWidth = (int)(totalWidth * ratio);
+                var newHeight = (int)(totalHeight * ratio);
+
+                var resizedBitmap = new Bitmap(newWidth, newHeight);
+                using (var graphics = Graphics.FromImage(resizedBitmap))
+                {
+                    graphics.CompositingQuality = CompositingQuality.HighSpeed;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.HighSpeed;
+
+                    // Draw the original image onto the resized bitmap
+                    graphics.DrawImage(bitmap, 0, 0, newWidth, newHeight);
+                }
+                resizedBitmap.Save(outputFilename, System.Drawing.Imaging.ImageFormat.Png);
+            }
+            else
+            {
                 bitmap.Save(outputFilename, System.Drawing.Imaging.ImageFormat.Png);
             }
         }
-
     }
+
 }
